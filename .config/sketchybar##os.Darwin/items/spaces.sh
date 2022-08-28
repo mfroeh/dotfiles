1d12dbf7dc549fa1bd97169f1fe3dc2e7b9aad90
mfroeh <mfroeh0@protonmail.com>
2022-08-28 19:39:10 +0200

    Modify sketchybar

--- .config/sketchybar##os.Darwin/items/spaces.sh
+++ .config/sketchybar##os.Darwin/items/spaces.sh
@@ -1,4 +1,4 @@
-SPACE_ICONS=("􀖇" "2" "3" "4" "5" "6" "7" "8" "9" "10")
+SPACE_ICONS=("􀋃" "􀎭" "􀇩" "􀪏" "􀌥" "6" "7" "8" "9" "10")
 
 for i in "${!SPACE_ICONS[@]}"
 do
@@ -6,13 +6,13 @@ do
     sketchybar --add space space.$sid left     \
         --set space.$sid associated_space=$sid     \
                          icon=${SPACE_ICONS[i]}                     \
-                         icon.padding_left=8                        \
-                         icon.padding_right=8                       \
+                         icon.padding_left=4                        \
+                         icon.padding_right=4                       \
                          background.padding_left=5                  \
                          background.padding_right=5                 \
                          background.color=0x44ffffff                \
                          background.corner_radius=5                 \
-                         background.height=22                       \
+                         background.height=27                 \
                          background.drawing=off                     \
                          label.drawing=off                          \
                          script="$PLUGIN_DIR/space.sh"              \
--- .config/sketchybar##os.Darwin/items/window_title.sh
+++ .config/sketchybar##os.Darwin/items/window_title.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
-sketchybar --add item space_separator left                            \
---add item window_title left                               \
+sketchybar --add item window_title left                               \
 --set window_title    script="$PLUGIN_DIR/window_title.sh" \
 icon.drawing=off                     \
 label.font="$FONT:Semibold:15.0"         \
