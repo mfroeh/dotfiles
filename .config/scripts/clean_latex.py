df8511b8fbd887c08338a6e2c97c6fafd26d6110
mfroeh <mfroeh0@protonmail.com>
2022-09-05 23:00:17 +0200

    Change yabai setup slightly

--- .config/scripts/clean_latex.py
+++ .config/scripts/clean_latex.py
@@ -4,4 +4,5 @@ from pathlib import Path
 
 for file in Path(f"{Path.home()}/uni/").glob("**/*"):
     if file.name.endswith((".aux", ".fdb_latexmk", ".fls", ".log", ".synctex.gz" )):
+        os.remove(file)
         print(file)
