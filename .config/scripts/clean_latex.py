e4d2ccf36bbd76af1cc9fb20f318407b2c4d181c
mfroeh <mfroeh0@pm.me>
2022-11-12 19:23:28 +0100

    Remove neofetch config and latex clean script

--- .config/scripts/clean_latex.py
+++
@@ -1,8 +0,0 @@
-from genericpath import isdir
-import os
-from pathlib import Path
-
-for file in Path(f"{Path.home()}/org/").glob("**/*"):
-    if file.name.endswith((".aux", ".fdb_latexmk", ".fls", ".log", ".synctex.gz", ".bbl")):
-        os.remove(file)
-        print(file)
