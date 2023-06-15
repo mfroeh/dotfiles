d470b806b411f81067a139fab19c020f490dbed6
mfroeh <mfroeh0@pm.me>
2023-06-15 21:56:14 +0200

    Add emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,76 @@
+;;; package-archives
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+	("elpa" . "https://elpa.gnu.org/packages/")))
+
+;;; bootstrap use-package
+(package-initialize)
+(setq use-package-always-ensure t)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile (require 'use-package))
+
+;;; undo for evil
+(use-package undo-fu
+  :ensure t)
+
+;;; evil
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-keybinding nil)
+  (setq evil-undo-system 'undo-fu)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-Y-yank-to-eol t)
+  :config
+  (evil-mode 1))
+
+;;; evil-collection
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :custom
+  (evil-collection-setup-minibuffer t)
+  :config
+  (evil-collection-init))
+
+;;; enable relative line numbers
+(global-display-line-numbers-mode t)
+(setq display-line-numbers-type 'relative)
+
+;;; modus themes
+(use-package modus-themes
+  :ensure t
+  :config
+  ;;; Add all your customizations prior to loading the themes
+  (setq modus-themes-italic-constructs t
+	modus-themes-bold-constructs nil)
+  ;;; Load the theme of your choice.
+  (load-theme 'modus-operandi t))
+
+;;; snippets
+(use-package yasnippet :ensure t)
+
+;;; company for completion
+(use-package company :ensure t)
+
+;;; lsp mode
+(use-package lsp-mode :ensure t
+  :hook
+  (c++-mode . lsp)
+  (c-mode . lsp)
+  (lsp-mode . lsp-enable-which-key-integration)
+  :commands lsp)
+
+;; optional if you want which-key integration
+(use-package which-key
+    :config
+    (which-key-mode))
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+;; if you are helm user
+;;(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+;;(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
